8a2339c84c6f10c1092a1f1141421f422aababf7
iGerJ02 <gjaimejuanguardia@gmail.com>
2023-05-04 00:51:33 +0200

    Fixed several bugs Added an ImageView for the map so we can calculate the rows and cols. Added a Listener to wait for the image Now the cellSize will be determined by the screensize in the file values/integers.xml. Using onResume

--- app/src/main/java/com/example/findhim/GameActivity.kt
+++ app/src/main/java/com/example/findhim/GameActivity.kt
@@ -5,11 +5,10 @@ import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
-import android.view.Window
+import android.view.ViewTreeObserver
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import java.lang.Integer.min
 import java.util.*
 
 
@@ -18,7 +17,9 @@ class GameActivity : AppCompatActivity() {
     private lateinit var textInput1: TextView
     private lateinit var gridContainer: FrameLayout
     private lateinit var backgroundImage: Drawable
+    private lateinit var bgImage: ImageView
     private lateinit var chronometer: Chronometer
+    private var cellSize: Int = 0
     private var clicks: Int = 0
     private var imageIndex: Int = -1
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,94 +29,119 @@ class GameActivity : AppCompatActivity() {
         textInput1 = findViewById(R.id.text_input1)
         gridContainer = findViewById(R.id.grid_container)
 
+        cellSize = resources.getInteger(R.integer.cell_size)
+
         // Get the input values passed from the MainActivity
         val message = intent.getStringExtra(MESSAGE_KEY)
         val image = intent.getStringExtra(SELECTED_LEVEL_IMAGE_KEY)
 
-        // Calculate the cell size based on the screen width and height
-        val displayMetrics = resources.displayMetrics
-        val screenWidth = displayMetrics.widthPixels
-        val screenHeight = displayMetrics.heightPixels
-        val cellSize = min(screenWidth, screenHeight) / 10 // You can adjust this value to suit your needs
-
-        // Calculate the number of rows and columns based on the screen size and the cell size
-        val numCols = 10
-        val numRows = 16
-        val numCells = numRows * numCols
-
-        // Display the input values in the UI
-        textInput1.text =
-            "Welcome to the Game! $message, gridContainer width: ${gridContainer.width} , gridContainer height: ${gridContainer.height} "
-
-        // Create a list of cell values
-        val random = Random()
-        if (imageIndex == -1) {
-            imageIndex = random.nextInt(numCells)
-        }
-        val cellValues =
-            Array(numCells) { if (it == imageIndex) R.drawable.wally else R.drawable.transparent_square }
-
-        // Create the grid of cells with random values
-        val adapter = object : BaseAdapter() {
-            override fun getCount(): Int {
-                return numCells
-            }
-
-            override fun getItem(position: Int): Any? {
-                return null
-            }
-
-            override fun getItemId(position: Int): Long {
-                return 0
-            }
-
-            override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-                val imageView: ImageView
-                if (convertView == null) {
-                    imageView = ImageView(applicationContext)
-                    imageView.layoutParams = ViewGroup.LayoutParams(cellSize, cellSize)
-                    imageView.scaleType = ImageView.ScaleType.CENTER_CROP
-                    imageView.setPadding(8, 8, 8, 8)
-                } else {
-                    imageView = convertView as ImageView
-                }
-
-                imageView.setImageResource(cellValues[position])
-                return imageView
-            }
-        }
-        // Set the number of rows and columns of the grid based on the calculated values
-        gridView.numColumns = numCols
-        gridView.verticalSpacing = 8
-        gridView.horizontalSpacing = 8
-        gridView.stretchMode = GridView.STRETCH_COLUMN_WIDTH
-        gridView.adapter = adapter
-
-        // Set onItemClick Listener for each cell to detect when the user clicks on it
-        gridView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            clicks++
-            // If the user clicks on the image, display a message and finish the activity
-            if (position == imageIndex) {
-                Toast.makeText(this, "You win!", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, FinalActivity::class.java)
-
-                setLogs(intent)
-                chronometer.stop()
-                startActivity(intent)
-            } else {
-                // Do something else if the user clicked on a cell without the letter
-                // For example, you can show a Toast message
-                Toast.makeText(this, "Try again!", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        // Set a random image as the background of the grid
+        bgImage = findViewById(R.id.map)
         val backgroundImageId = intent.getIntExtra(SELECTED_LEVEL_IMAGE_KEY, 0)
         backgroundImage = ContextCompat.getDrawable(this, backgroundImageId)!!
-        gridView.background = backgroundImage
+        bgImage.setImageResource(backgroundImageId)
 
-        chronometer = findViewById(R.id.chronometer)
-        chronometer.start()
+    }
+
+    override fun onResume() {
+        var finalHeight = 0
+        var finalWidth = 0
+        val message = intent.getStringExtra(MESSAGE_KEY)
+
+        //We will use a listener to wait for the image to be drawn and calculate the cells
+        val vto: ViewTreeObserver = bgImage.viewTreeObserver
+        vto.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
+            override fun onPreDraw(): Boolean {
+                // Remove after the first run so it doesn't fire forever
+                bgImage.viewTreeObserver.removeOnPreDrawListener(this)
+
+                finalHeight = bgImage.height
+                finalWidth = bgImage.width
+                val numCols = finalWidth / cellSize
+
+                gridView.columnWidth = cellSize
+                val numRows = finalHeight / cellSize
+                val numCells = numRows * numCols
+
+
+                // Display the input values in the UI
+                textInput1.text =
+                    "Welcome to the Game! $message, gridContainer width: ${finalWidth} , gridContainer height: ${finalHeight} "
+
+                // Create a list of cell values
+                val random = Random()
+                if (imageIndex == -1) {
+                    imageIndex = random.nextInt(numCells)
+                }
+                val cellValues =
+                    Array(numCells) { if (it == imageIndex) R.drawable.wally else R.drawable.transparent_square }
+
+                // Create the grid of cells with random values
+                val adapter = object : BaseAdapter() {
+                    override fun getCount(): Int {
+                        return numCells
+                    }
+
+                    override fun getItem(position: Int): Any? {
+                        return null
+                    }
+
+                    override fun getItemId(position: Int): Long {
+                        return 0
+                    }
+
+                    override fun getView(
+                        position: Int,
+                        convertView: View?,
+                        parent: ViewGroup?
+                    ): View {
+                        val imageView: ImageView
+                        if (convertView == null) {
+                            imageView = ImageView(applicationContext)
+                            imageView.layoutParams = ViewGroup.LayoutParams(cellSize, cellSize)
+                            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+                            imageView.setPadding(8, 8, 8, 8)
+                        } else {
+                            imageView = convertView as ImageView
+                        }
+
+                        imageView.setImageResource(cellValues[position])
+                        return imageView
+                    }
+                }
+                // Set the number of rows and columns of the grid based on the calculated values
+                gridView.numColumns = numCols
+                gridView.stretchMode = GridView.STRETCH_COLUMN_WIDTH
+                gridView.adapter = adapter
+
+                // Set onItemClick Listener for each cell to detect when the user clicks on it
+                gridView.onItemClickListener =
+                    AdapterView.OnItemClickListener { _, _, position, _ ->
+                        clicks++
+                        // If the user clicks on the image, display a message and finish the activity
+                        if (position == imageIndex) {
+                            Toast.makeText(this@GameActivity, "You win!", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this@GameActivity, FinalActivity::class.java)
+
+                            setLogs(intent)
+                            chronometer.stop()
+                            startActivity(intent)
+                        } else {
+                            // Do something else if the user clicked on a cell without the letter
+                            // For example, you can show a Toast message
+                            Toast.makeText(this@GameActivity, "Try again!", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+
+                chronometer = findViewById(R.id.chronometer)
+                chronometer.start()
+
+                return true
+            }
+        })
+
+
+        super.onResume()
     }
 
     override fun onDestroy() {
