dab9fab782f709b73b1e4ece07ac7af3c46276d5
Sebastian <sjitaru29@gmail.com>
2023-05-27 14:22:09 +0200

    LogImproved

--- app/src/main/java/com/example/findhim/persistency/GameViewHolder.kt
+++ app/src/main/java/com/example/findhim/persistency/GameViewHolder.kt
@@ -6,12 +6,21 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.findhim.R
+import com.example.findhim.databinding.LogLayoutBinding
 
 class GameViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private val playerNameView: TextView = itemView.findViewById(R.id.playerName)
 
-    fun bind(text: String?) {
-        playerNameView.text = text
+
+    private val playerNameView: TextView = itemView.findViewById(R.id.playerName)
+    private val gameTime: TextView = itemView.findViewById(R.id.gameTime)
+    private val cliks:TextView = itemView.findViewById(R.id.clicks)
+    private val id:TextView = itemView.findViewById(R.id.gameId)
+
+    fun bind(game: Game) {
+        playerNameView.text = game.nickname
+        gameTime.text = game.gameTime
+        cliks.text = game.clicks
+        id.text = game.id.toString()
     }
 
     companion object {
